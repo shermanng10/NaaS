558e32fc6af9787ad497562c39e7d15ecce156a3
shermanng10 <sherman.ng10@gmail.com>
2016-09-02 23:44:40 -0400

    Use class based configurations

--- config.py
+++ config.py
@@ -1,2 +1,14 @@
-DEBUG = True
-DB_STRING = 'dbname=naas user=postgres host=localhost'
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    DB_STRING = 'dbname=naas user=postgres host=localhost'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+    LOGGER_HANDLER_POLICY = 'never'
+    DB_STRING = 'dbname=testdb user=postgres host=localhost'
